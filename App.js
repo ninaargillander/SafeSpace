17dbf2b57e4dad12f2349022a2d168a74a17bc80
Nina Argillander <nina@argillander.se>
2020-03-09 17:04:39 +0100

    Basic chat UI implemented

--- App.js
+++ App.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import styles from './Sass/App.scss'
+import {
+  Platform,
+  Text,
+  View,
+  Linking,
+  TextInput,
+  Button,
+  KeyboardAvoidingView
+} from 'react-native';
+import appStyles from './Sass/app.scss';
+import chatStyles from './Sass/chat.scss';
 
-const instructions = Platform.select({
+/* const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu'
+}); */
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userName: 'Kalle Kula'
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <KeyboardAvoidingView style={appStyles.container} behavior="padding">
+        <Text style={appStyles.header}>{this.state.userName}</Text>
+        <Text style={chatStyles.msgSent}>To get started, edit App.js</Text>
+        <Text style={chatStyles.msgRecieved}>To get started, edit App.js</Text>
+
+        <View style={chatStyles.inputBox}>
+          <TextInput style={chatStyles.textInput}></TextInput>
+          <Button title="Skicka" style={chatStyles.sendButton}></Button>
+        </View>
+      </KeyboardAvoidingView>
     );
   }
 }
-
-
-
