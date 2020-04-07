c436dbd6b5ff8bacf91907d601a50333a2da7d60
Nina Argillander <nina@argillander.se>
2020-04-07 14:12:44 +0200

    Almost working chat, missing send message to server

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import io from 'socket.io-client';
 
 import {
   Platform,
@@ -9,143 +8,21 @@ import {
   TextInput,
   Button,
   KeyboardAvoidingView,
-  FlatList
+  FlatList,
 } from 'react-native';
 
 import Header from './Components/header.js';
+import ChatView from './Views/chatView.js';
 
-import appStyles from './Sass/app.scss';
+import appStyles from './Sass/App.scss';
 import chatStyles from './Sass/chat.scss';
 
 export default class App extends Component {
-
-
   constructor(props) {
     super(props);
-
-    this.state = {
-      userName: 'Kalle Kula',
-      currMsg: "",
-      messages: []
-    };
-
-    this.submitChatMessage = this.submitChatMessage.bind(this);
   }
 
-  componentDidMount() {
-    this.socket = io('http://192.168.10.159:8000');
-    this.socket.emit('init', {
-      senderId: "5e843ddbbd8a99081cd3f613",
-    });
-  }
-
-  componentWillUnmount() {
-    this.socket.emit('disconnect', {
-      senderId: "5e843ddbbd8a99081cd3f613",
-    });
-  }
-
-  submitChatMessage() {
-    this.socket.emit('message', this.state.currMsg);
-    console.log(`Message sent: ${this.state.currMsg}`);
-    this.setState({ currMsg: "" });
-    this.textInput.clear();
-  }
-
-  // FlatList, TextInput och Button bör kunna göras till en component (chatView) men det funkar inte nu
   render() {
-    return (
-      //Finns bättre sätt än <KeyBoardAvoidingView> för att få allt att anpassas då tangentbordet öppnas
-      <KeyboardAvoidingView style={appStyles.container} behavior="padding">
-        <Header title={this.state.userName} />
-
-        <FlatList
-          ref={el => (this.list = el)}
-          data={DATA}
-          renderItem={({ item }) => <Item msg={item} />}
-          keyExtractor={item => item.id}
-          initialScrollIndex={DATA.length - 1} // Gör att man hamnar längst ner i konversationen och får scrolla uppåt, gissar på att det inte kommer funka när data hämtas från db?
-        />
-
-        <View style={chatStyles.inputBox}>
-          <TextInput
-            ref={input => {
-              this.textInput = input;
-            }}
-            style={chatStyles.textInput}
-            onChangeText={currMsg => this.setState({ currMsg })}
-          />
-
-          <Button
-            color="#133b43"
-            title="Skicka"
-            style={chatStyles.sendButton}
-            onPress={() => {
-              this.submitChatMessage(this.state.currMsg);
-            }}
-          />
-        </View>
-      </KeyboardAvoidingView>
-    );
+    return <ChatView></ChatView>;
   }
 }
-
-function Item({ msg }) {
-  if (msg.recieved) {
-    return (
-      <View style={[chatStyles.msg, chatStyles.msgRecieved]}>
-        <Text>{msg.message}</Text>
-      </View>
-    );
-  }
-
-  return (
-    <View style={[chatStyles.msg, chatStyles.msgSent]}>
-      <Text>{msg.message}</Text>
-    </View>
-  );
-}
-
-const DATA = [
-  {
-    message:
-      'Meddelande 0: To get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo  get started, edit App.jsTo get started, edit App.jsTo get started, edit App.js',
-    recieved: true,
-    id: 0
-  },
-  {
-    message:
-      'Meddelande 1: To get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo  get started, edit App.jsTo get started, edit App.jsTo get started, edit App.js',
-    recieved: false,
-    id: 1
-  },
-  {
-    message:
-      'Meddelande 2: To get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo  get started, edit App.jsTo get started, edit App.jsTo get started, edit App.js',
-    recieved: true,
-    id: 2
-  },
-  {
-    message:
-      'Meddelande 3: To get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo  get started, edit App.jsTo get started, edit App.jsTo get started, edit App.js',
-    recieved: false,
-    id: 3
-  },
-  {
-    message:
-      'Meddelande 4: To get started, edit App.jsTo  get started, edit App.jsTo get started, edit App.jsTo get started, edit App.js',
-    recieved: true,
-    id: 4
-  },
-  {
-    message:
-      'Meddelande 5: To get started, edit App.jsTo  get started, edit App.jsTo get started, edit App.jsTo get started, edit App.js',
-    recieved: true,
-    id: 5
-  },
-  {
-    message: 'Meddelande 6: To gets',
-    recieved: false,
-    id: 6
-  }
-];
--- Views/chatView.js
+++ Views/chatView.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
+
 import {
+  Platform,
   Text,
   View,
+  Linking,
   TextInput,
   Button,
-  FlatList,
   KeyboardAvoidingView,
-  ScrollView
+  FlatList,
 } from 'react-native';
 
+import Header from '../Components/header.js';
+
+import appStyles from '../Sass/App.scss';
 import chatStyles from '../Sass/chat.scss';
 
 export default class ChatView extends Component {
@@ -16,47 +22,100 @@ export default class ChatView extends Component {
     super(props);
 
     this.state = {
-      currMsg: ''
+      userName: 'Kalle Kula',
+      currUser: '',
+      currMsg: '',
+      messages: [],
+      loading: true,
     };
 
-    this.pressSend = this.pressSend.bind(this);
-    this.onChange = this.onChange.bind(this);
+    this.submitChatMessage = this.submitChatMessage.bind(this);
   }
 
-  pressSend() {
+  async componentDidMount() {
+    try {
+      const callChatApi = await fetch(
+        'http://192.168.1.207:8000/conversations/5e68c508c18e2a00ee6bf0f8 '
+      );
+      callChatApi.json().then((data) => {
+        this.setState({ messages: data, loading: false });
+      });
+
+      this.socket = io('http://192.168.1.207:8000');
+      this.socket.emit('init', {
+        senderId: '5e843ddbbd8a99081cd3f613',
+      });
+    } catch (err) {
+      console.log('Error fetching data', err);
+    }
+
+    this.setState({ currUser: 'päron' }); //TODO: Här måste userId kunna hämtas
+  }
+
+  componentWillUnmount() {
+    this.socket.emit('disconnect', {
+      senderId: '5e843ddbbd8a99081cd3f613',
+    });
+  }
+
+  submitChatMessage() {
+    this.socket.emit('message', this.state.currMsg);
+    console.log(`Message sent: ${this.state.currMsg}`);
+    this.setState({ currMsg: '' });
     this.textInput.clear();
-  }
-
-  onChange(text) {
-    this.setState({ currMsg: text });
   }
 
   render() {
     return (
-      <KeyboardAvoidingView>
+      //Finns bättre sätt än <KeyBoardAvoidingView> för att få allt att anpassas då tangentbordet öppnas
+      <KeyboardAvoidingView style={appStyles.container} behavior="padding">
+        <Header title={this.state.userName} />
+
         <FlatList
-          data={this.props.data}
-          renderItem={({ item }) => <Item msg={item} />}
-          keyExtractor={item => item.id}
-          initialScrollIndex={this.props.data.length - 1} // Gör att man hamnar längst ner i konversationen och får scrolla uppåt, gissar på att det inte kommer funka när data hämtas från db?
+          ref={(el) => (this.list = el)}
+          data={this.state.messages}
+          renderItem={({ item }) => (
+            <Item msg={item} currUser={this.state.currUser} />
+          )}
+          keyExtractor={(item) => item.id} //TODO: Unikt ID för alla meddelanden
+          // initialScrollIndex={DATA.length - 1} // Gör att man hamnar längst ner i konversationen och får scrolla uppåt, gissar på att det inte kommer funka när data hämtas från db?
         />
+
+        <View style={chatStyles.inputBox}>
+          <TextInput
+            ref={(input) => {
+              this.textInput = input;
+            }}
+            style={chatStyles.textInput}
+            onChangeText={(currMsg) => this.setState({ currMsg })}
+          />
+
+          <Button
+            color="#133b43"
+            title="Skicka"
+            style={chatStyles.sendButton}
+            onPress={() => {
+              this.submitChatMessage(this.state.currMsg);
+            }}
+          />
+        </View>
       </KeyboardAvoidingView>
     );
   }
 }
 
-function Item({ msg }) {
-  if (msg.recieved) {
+function Item({ msg, currUser }) {
+  if (msg.userId != currUser) {
     return (
       <View style={[chatStyles.msg, chatStyles.msgRecieved]}>
-        <Text>{msg.message}</Text>
+        <Text>{msg.text}</Text>
       </View>
     );
   }
 
   return (
     <View style={[chatStyles.msg, chatStyles.msgSent]}>
-      <Text>{msg.message}</Text>
+      <Text>{msg.text}</Text>
     </View>
   );
 }
