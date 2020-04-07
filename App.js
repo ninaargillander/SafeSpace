c22a54989a4dc97b79faf093d0e30811c7da06ba
louisenilsson <louisenilsson@users.noreply.github.com>
2020-04-07 08:36:47 +0200

    contacts - a start

--- App.js
+++ App.js
@@ -1,38 +1,90 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import {
+  Platform,
+  Text,
+  View,
+  Linking,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
+  FlatList
+} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+//import {NavigationContainer} from '@react-navigation/native'
+import Header from './Components/header.js';
+
+//import appStyles from './Sass/app.scss';
+import chatStyles from './Sass/chat.scss';
 
 export default class App extends Component {
+  // static navigationOptions = {
+  //   navigation: any,
+  // };
+
+  static navigationOptions = ({navigation}) => ({
+    title:'Contacts',
+    headerLeft: (
+        <Button onPress={() =>  {
+            navigation.navigate('Chat')
+        }}
+        
+        />
+    ),
+});
+  // FlatList, TextInput och Button bör kunna göras till en component (chatView) men det funkar inte nu
   render() {
+   
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+      <Header title={'Contacts'} />
+      <FlatList
+          ref={el => (this.list = el)}
+          data={DATA}
+          renderItem={({ item }) => <Item msg={item} />}
+          keyExtractor={item => item.id}
+          initialScrollIndex={DATA.length - 1} // Gör att man hamnar längst ner i konversationen och får scrolla uppåt, gissar på att det inte kommer funka när data hämtas från db?
+        />
+      <Button title="Kalle Kula" onPress={() => alert('se konvo')}/>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+
+
+function Item({ msg }) {
+  
+
+  return (
+    <View style={[chatStyles.msg]}>
+      <Text>{msg.user}</Text>
+    </View>
+  );
+}
+
+const DATA = [
+  {
+    user:
+      'Kalle Kula',
+    id: 0
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  
+  {
+    user:
+      'Lisa Avlång',
+    id: 1
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  
+  {
+    user:
+      'Anna Asbra',
+    id: 2
   },
-});
+  
+  {
+    user:
+      'Fidde Framåt',
+    id: 3
+  }
+  
+];
---
+++ View/Chat.js
@@ -0,0 +1,144 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  Text,
+  View,
+  Linking,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
+  FlatList
+} from 'react-native';
+
+
+import Header from './Components/header.js';
+
+import appStyles from '../Sass/app.scss';
+import chatStyles from '../Sass/chat.scss';
+
+export default class Chat extends Component {
+    // static navigationOptions = ({navigation}) => ({
+    //     title:'Chat',
+    //     headerLeft: (
+    //         <Button onPress={() =>  {
+    //             navigation.navigate('Contacts')
+    //         }}
+    //         />
+    //     ),
+    // });
+    
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userName: 'Kalle Kula',
+      currMsg: '',
+      messages: []
+    };
+
+    this.pressSend = this.pressSend.bind(this);
+  }
+
+  pressSend(text) {
+    console.log(text);
+    this.textInput.clear();
+  }
+
+  // FlatList, TextInput och Button bör kunna göras till en component (chatView) men det funkar inte nu
+  render() {
+    return (
+      //Finns bättre sätt än <KeyBoardAvoidingView> för att få allt att anpassas då tangentbordet öppnas
+      <KeyboardAvoidingView style={appStyles.container} behavior="padding">
+        <Header title={this.state.userName} />
+        <Button title="Tillbaka" onPress={() => alert('Gå tbx till kontaker')}/>
+
+        <FlatList
+          ref={el => (this.list = el)}
+          data={DATA}
+          renderItem={({ item }) => <Item msg={item} />}
+          keyExtractor={item => item.id}
+          initialScrollIndex={DATA.length - 1} // Gör att man hamnar längst ner i konversationen och får scrolla uppåt, gissar på att det inte kommer funka när data hämtas från db?
+        />
+
+        <View style={chatStyles.inputBox}>
+          <TextInput
+            ref={input => {
+              this.textInput = input;
+            }}
+            style={chatStyles.textInput}
+            onChangeText={currMsg => this.setState({ currMsg })}
+          />
+
+          <Button
+            color="#133b43"
+            title="Skicka"
+            style={chatStyles.sendButton}
+            onPress={() => {
+              this.pressSend(this.state.currMsg);
+            }}
+          />
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+function Item({ msg }) {
+  if (msg.recieved) {
+    return (
+      <View style={[chatStyles.msg, chatStyles.msgRecieved]}>
+        <Text>{msg.message}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={[chatStyles.msg, chatStyles.msgSent]}>
+      <Text>{msg.message}</Text>
+    </View>
+  );
+}
+
+const DATA = [
+  {
+    message:
+      'Meddelande 0: To get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo  get started, edit App.jsTo get started, edit App.jsTo get started, edit App.js',
+    recieved: true,
+    id: 0
+  },
+  {
+    message:
+      'Meddelande 1: To get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo  get started, edit App.jsTo get started, edit App.jsTo get started, edit App.js',
+    recieved: false,
+    id: 1
+  },
+  {
+    message:
+      'Meddelande 2: To get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo  get started, edit App.jsTo get started, edit App.jsTo get started, edit App.js',
+    recieved: true,
+    id: 2
+  },
+  {
+    message:
+      'Meddelande 3: To get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo get started, edit App.jsTo  get started, edit App.jsTo get started, edit App.jsTo get started, edit App.js',
+    recieved: false,
+    id: 3
+  },
+  {
+    message:
+      'Meddelande 4: To get started, edit App.jsTo  get started, edit App.jsTo get started, edit App.jsTo get started, edit App.js',
+    recieved: true,
+    id: 4
+  },
+  {
+    message:
+      'Meddelande 5: To get started, edit App.jsTo  get started, edit App.jsTo get started, edit App.jsTo get started, edit App.js',
+    recieved: true,
+    id: 5
+  },
+  {
+    message: 'Meddelande 6: To gets',
+    recieved: false,
+    id: 6
+  }
+];
